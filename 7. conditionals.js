da2e6396c58807b1c6c3d3e8762178bba76c401a
akhilcoditas <akhil.sachan@coditas.com>
2024-02-06 12:17:54 +0530

    conditionals

---
+++ 7. conditionals.js
@@ -0,0 +1,10 @@
+const age = 89;
+const minimumAgeAllowedInSectionOne = 18;
+const minimumAgeAllowedInSectionTwo = 24;
+
+if (age >= minimumAgeAllowedInSectionOne) {
+  console.log("Allowed in section 1");
+}
+// assignment -> done
+
+// switch -> covered in live class
